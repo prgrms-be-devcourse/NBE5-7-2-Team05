9229985536f7031a849df44aec586b5e3ad7f153
Taeho KIM <laferrari963@naver.com>
2025-05-08 14:38:11 +0900

    Feat(CommentController): 댓글 게시 컨트롤러 구현

---
+++ src/main/java/io/powerrangers/backend/controller/CommentController.java
@@ -0,0 +1,31 @@
+package io.powerrangers.backend.controller;
+
+import io.powerrangers.backend.dto.comment.CommentCreateRequestDto;
+import io.powerrangers.backend.service.CommentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    /***
+     *
+     * @param request <- 현재 userid를 requestbody로 받는중. 추후 리팩토링
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<String> createComment(@RequestBody CommentCreateRequestDto request){
+        commentService.createComment(request.getUserId(),request);
+        return ResponseEntity.ok("Comment created");
+    }
+}
