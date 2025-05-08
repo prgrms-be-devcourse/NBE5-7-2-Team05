d311554c92b803c62b0a5df614ca2e805a051f29
Taeho KIM <laferrari963@naver.com>
2025-05-08 15:20:03 +0900

    Refactor(CommentRepository): 불필요 어노테이션 제거

--- src/main/java/io/powerrangers/backend/repository/CommentRepository.java
+++ src/main/java/io/powerrangers/backend/repository/CommentRepository.java
@@ -2,11 +2,9 @@ package io.powerrangers.backend.repository;
 
 import io.powerrangers.backend.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     Optional<Comment> findById(Long id);
 }
