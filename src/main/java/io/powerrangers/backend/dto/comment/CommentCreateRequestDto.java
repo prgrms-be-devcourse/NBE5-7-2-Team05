1df747a7acb76a1d7590b6df1475ed8839f6a63e
Taeho KIM <laferrari963@naver.com>
2025-05-08 14:39:52 +0900

    Feat(CommentCreateRequestDto): dto생성

---
+++ src/main/java/io/powerrangers/backend/dto/comment/CommentCreateRequestDto.java
@@ -0,0 +1,23 @@
+package io.powerrangers.backend.dto.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class CommentCreateRequestDto {
+    @NotNull(message = "유저가 존재하지 않습니다.")
+    private final Long taskId;
+
+    @NotNull(message = "할 일이 존재하지 않습니다.")
+    private final Long userId;
+
+    private final Long parentId;
+
+    @NotBlank(message = "내용을 입력해주세요")
+    private final String content;
+}
