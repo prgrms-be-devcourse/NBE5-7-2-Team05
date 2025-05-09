806023e4ba3c23a3732eea3786ae525afb22c240
silver-eunjoo <leeeun5423@naver.com>
2025-05-09 10:21:45 +0900

    Refactor : 코드 컨벤션 반영 (final, DTO)

--- src/main/java/io/powerrangers/backend/dto/FollowResponseDto.java
+++ src/main/java/io/powerrangers/backend/dto/FollowResponseDto.java
@@ -3,19 +3,14 @@ package io.powerrangers.backend.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class FollowResponseDto {
 
     private final Long followId;
     private final Long followerId;
     private final Long followingId;
 
-    @Builder
-    public FollowResponseDto(Long followId, Long followerId, Long followingId) {
-        this.followId = followId;
-        this.followerId = followerId;
-        this.followingId = followingId;
-    }
 }
