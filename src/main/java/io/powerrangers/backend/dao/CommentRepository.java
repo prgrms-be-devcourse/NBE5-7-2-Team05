efb021df8c87e11ef939680bac4ed2575b73ccab
Taeho KIM <laferrari963@naver.com>
2025-05-09 12:38:01 +0900

    Update CommentRepository.java

--- src/main/java/io/powerrangers/backend/dao/CommentRepository.java
+++ src/main/java/io/powerrangers/backend/dao/CommentRepository.java
@@ -6,12 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    Optional<Comment> findById(Long id);
 
+public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.task.id = :taskId")
     List<Comment> findByTaskId(@Param("taskId")Long taskId);
 }
