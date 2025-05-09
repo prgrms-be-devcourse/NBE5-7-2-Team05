33489a68d55779d22ff89754e39dbda7bf4a6bdd
jwh946 <garcon0406@gmail.com>
2025-05-09 15:38:15 +0900

    Refactor: repository 폴더명 변경

--- src/main/java/io/powerrangers/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.powerrangers.backend.repository;
-
-import io.powerrangers.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
