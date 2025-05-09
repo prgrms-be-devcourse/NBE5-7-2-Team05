939169c62efaa00ceef2648066ba36b26e6ccc5a
sleepyhoon <pokjm99@naver.com>
2025-05-09 16:29:11 +0900

    Refactor: authorization 첫글자를 대문자로 변경

--- src/main/java/io/powerrangers/backend/config/JwtAuthenticationFilter.java
+++ src/main/java/io/powerrangers/backend/config/JwtAuthenticationFilter.java
@@ -65,7 +65,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("authorization");
+        String bearerToken = request.getHeader("Authorization");
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
