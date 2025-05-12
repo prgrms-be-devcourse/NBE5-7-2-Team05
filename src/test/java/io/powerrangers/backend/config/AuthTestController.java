8810867f3d647c879f37a3b358524aa7805569fc
sleepyhoon <pokjm99@naver.com>
2025-05-12 14:43:01 +0900

    Test: Filter test 추가

--- src/test/java/io/powerrangers/backend/config/AuthTestController.java
+++ src/test/java/io/powerrangers/backend/config/AuthTestController.java
@@ -1,18 +1,16 @@
 package io.powerrangers.backend.config;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class AuthTestController {
     @GetMapping("/security-endpoint")
     public String securityEndpoint() {
         return "This is a security endpoint!";
     }
 
-    @GetMapping("/url")
+    @GetMapping("/test/url")
     public String testUrl() {
         return "This is a public endpoint";
     }
--- src/test/java/io/powerrangers/backend/config/JwtAuthenticationFilterTest.java
+++ src/test/java/io/powerrangers/backend/config/JwtAuthenticationFilterTest.java
@@ -1,7 +1,10 @@
 package io.powerrangers.backend.config;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import io.powerrangers.backend.dto.Role;
@@ -57,8 +60,33 @@ class JwtAuthenticationFilterTest {
         when(jwtProvider.parseToken(token)).thenReturn(tokenBody);
         when(oauth2UserService.getUserDetails(tokenBody.getUserId())).thenReturn(userDetails);
 
-        mockMvc.perform(get("/test/security-endpoint")
+        mockMvc.perform(get("/test/url")
                         .header("Authorization", "Bearer validToken"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("잘못된 토큰으로 필터에서 RuntimeException 발생 검증")
+    void shouldThrowRuntimeExceptionForInvalidToken() {
+        String token = "invalid token";
+        when(jwtProvider.validateToken(token)).thenReturn(false);
+
+        assertThatThrownBy(() ->
+                mockMvc.perform(get("/security-endpoint") // 인증 필요한 경로
+                        .header("Authorization", "Bearer " + token))
+        )
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("문제가 있는 토큰입니다.");
+    }
+
+    @Test
+    @DisplayName("토큰이 없어 필터에서 RuntimeException 발생 검증")
+    void shouldThrowRuntimeExceptionForNullToken() throws Exception {
+        assertThatThrownBy(() ->
+                mockMvc.perform(get("/security-endpoint") // 인증 필요한 경로
+                )
+        )
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("문제가 있는 토큰입니다.");
     }
 }
