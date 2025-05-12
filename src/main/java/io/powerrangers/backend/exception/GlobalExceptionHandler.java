5b7008aa8bf387eab2f356324eb0012a774e7415
silver-eunjoo <leeeun5423@naver.com>
2025-05-12 14:27:21 +0900

    Feat : Valid 검사 실패해서 발생한 MethodArgumentNotValidException 핸들러에서 default Message 출력하도록 구현

--- src/main/java/io/powerrangers/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/io/powerrangers/backend/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package io.powerrangers.backend.exception;
 
 import io.powerrangers.backend.dto.BaseResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,14 +21,21 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({MethodArgumentNotValidException.class})
     protected ResponseEntity<BaseResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+
+        String errorMessage = fieldErrors.stream()
+                .map(error -> error.getField() + " : " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
         ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
-        return BaseResponse.error(errorCode.getMessage(), errorCode.getStatus());
+
+        return BaseResponse.error(errorMessage, errorCode.getStatus());
     }
 
     @ExceptionHandler({HttpMessageNotReadableException.class})
     protected ResponseEntity<BaseResponse<?>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
-        return BaseResponse.error(errorCode.getMessage(), errorCode.getStatus());
+        return BaseResponse.error(ErrorCode.INVALID_REQUEST.getMessage(), ErrorCode.INVALID_REQUEST.getStatus());
     }
 
     @ExceptionHandler({Exception.class})
