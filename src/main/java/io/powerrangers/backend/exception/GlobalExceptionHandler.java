15925fb2758ec7e2a9de6c154c60e904875a7e6b
silver-eunjoo <leeeun5423@naver.com>
2025-05-12 10:15:20 +0900

    Feat : GlobalExceptionHandler 구현

--- src/main/java/io/powerrangers/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/io/powerrangers/backend/exception/GlobalExceptionHandler.java
@@ -1,11 +1,36 @@
 package io.powerrangers.backend.exception;
 
+import io.powerrangers.backend.global.BaseResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler({CustomException.class})
+    protected ResponseEntity<BaseResponse<?>> handleCustomException(CustomException e) {
+        return BaseResponse.error(e.getErrorCode().getMessage(), HttpStatus.valueOf(e.getErrorCode().getStatus()));
+    }
 
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ResponseEntity<BaseResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
+        return BaseResponse.error(errorCode.getMessage(), HttpStatus.valueOf(errorCode.getStatus()));
+    }
 
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    protected ResponseEntity<BaseResponse<?>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ErrorCode errorCode = ErrorCode.INVALID_REQUEST;
+        return BaseResponse.error(errorCode.getMessage(), HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler({Exception.class})
+    protected ResponseEntity<BaseResponse<?>> handleException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+        return BaseResponse.error(errorCode.getMessage(), HttpStatus.valueOf(errorCode.getStatus()));
+    }
 }
