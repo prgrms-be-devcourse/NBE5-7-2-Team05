4837400bac164309d12a9a03e5dc908289f0b77e
sleepyhoon <pokjm99@naver.com>
2025-05-12 17:14:58 +0900

    Feat: 네이버 로그인 구현

--- src/main/java/io/powerrangers/backend/service/UserDetailsFactory.java
+++ src/main/java/io/powerrangers/backend/service/UserDetailsFactory.java
@@ -2,6 +2,7 @@ package io.powerrangers.backend.service;
 
 import io.powerrangers.backend.dto.UserDetails;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @SuppressWarnings("unchecked")
@@ -25,6 +26,16 @@ public class UserDetailsFactory {
                         .email(attributes.get("id").toString() + "@kakao.com")
                         .providerId(attributes.get("id").toString())
                         .profileImage(properties.get("profile_image"))
+                        .attributes(attributes)
+                        .build();
+            }
+            case "naver" -> {
+                Map<String, String> response = (Map<String, String>) attributes.get("response");
+                return UserDetails.builder()
+                        .name(response.get("name"))
+                        .email(response.get("email"))
+                        .providerId(response.get("id"))
+                        .profileImage(response.get("profile_image"))
                         .attributes(attributes)
                         .build();
             }
