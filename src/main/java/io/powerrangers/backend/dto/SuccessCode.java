98a9b2dad0ecf57e6b217408defc2c448c622c78
silver-eunjoo <leeeun5423@naver.com>
2025-05-12 17:31:45 +0900

    Refactor : Code enum에서 int가 아닌 HttpStatus로 넘겨주기

--- src/main/java/io/powerrangers/backend/dto/SuccessCode.java
+++ src/main/java/io/powerrangers/backend/dto/SuccessCode.java
@@ -9,17 +9,16 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     // 200 OK
-    MODIFIED_SUCCESS(200, "수정이 성공적으로 수행되었습니다."),
-    GET_SUCCESS(200, "데이터를 성공적으로 가져왔습니다."),
-    DELETED_SUCCESS(200, "삭제가 성공적으로 수행되었습니다."),
+    MODIFIED_SUCCESS(HttpStatus.OK, "수정이 성공적으로 수행되었습니다."),
+    GET_SUCCESS(HttpStatus.OK, "데이터를 성공적으로 가져왔습니다."),
 
     // 201 Created
-    ADDED_SUCCESS(201, "데이터가 성공적으로 추가되었습니다.");
+    ADDED_SUCCESS(HttpStatus.CREATED, "데이터가 성공적으로 추가되었습니다."),
 
-    private final int status;
+    // 204 No Content
+    DELETED_SUCCESS(HttpStatus.NO_CONTENT, "삭제가 성공적으로 수행되었습니다.");
+
+    private final HttpStatus status;
     private final String message;
 
-    public HttpStatus getStatus() {
-        return HttpStatus.valueOf(status);
-    }
 }
--- src/main/java/io/powerrangers/backend/exception/ErrorCode.java
+++ src/main/java/io/powerrangers/backend/exception/ErrorCode.java
@@ -9,32 +9,28 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     // 400 Bad Request
-    INVALID_REQUEST(400, "요청 데이터가 올바르지 않습니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청 데이터가 올바르지 않습니다."),
 
     // 401 Unauthorized
-    UNAUTHORIZED(401, "유효하지 않은 인증입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "유효하지 않은 인증입니다."),
 
     // 403 Forbidden
-    NOT_THE_OWNER(403,"할 일에 대한 권한이 없습니다."),
+    NOT_THE_OWNER(HttpStatus.UNAUTHORIZED,"할 일에 대한 권한이 없습니다."),
 
     // 404 Not Found
-    USER_NOT_FOUND(404, "존재하지 않는 사용자입니다."),
-    TASK_NOT_FOUND(404, "존재하지 않는 할 일입니다."),
-    COMMENT_NOT_FOUND(404, "존재하지 않는 댓글입니다."),
-    FOLLOW_NOT_FOUND(404, "팔로우 관계를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 할 일입니다."),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다."),
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "팔로우 관계를 찾을 수 없습니다."),
 
     // 409 Conflict
-    DUPLICATED_NICKNAME(409, "이미 존재하는 닉네임입니다."),
-    ALREADY_FOLLOWED(409, "이미 팔로우한 사용자입니다."),
+    DUPLICATED_NICKNAME(HttpStatus.CONFLICT, "이미 존재하는 닉네임입니다."),
+    ALREADY_FOLLOWED(HttpStatus.CONFLICT, "이미 팔로우한 사용자입니다."),
 
     // 500 INTERNAL_SERVER ERROR
-    INTERNAL_SERVER_ERROR(500,"서버 에러입니다. 서버 팀에게 문의해주세요.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"서버 에러입니다. 서버 팀에게 문의해주세요.");
 
-    private final int status;
+    private final HttpStatus status;
     private final String message;
-
-    public HttpStatus getStatus() {
-        return HttpStatus.valueOf(status);
-    }
 
 }
