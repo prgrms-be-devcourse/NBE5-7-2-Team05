0cfedb64878c9112c53e6b4b4357fe7a3d8388d5
silver-eunjoo <leeeun5423@naver.com>
2025-05-12 10:09:51 +0900

    Feat : CustomException 정의

---
+++ src/main/java/io/powerrangers/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package io.powerrangers.backend.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+
+}
