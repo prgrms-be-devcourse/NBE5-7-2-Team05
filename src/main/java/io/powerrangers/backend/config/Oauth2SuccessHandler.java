9a5a61c4692d4c64c8d170633822a21357d8c3e8
Taeho KIM <laferrari963@naver.com>
2025-05-18 03:20:54 +0900

    Update Oauth2SuccessHandler.java

--- src/main/java/io/powerrangers/backend/config/Oauth2SuccessHandler.java
+++ src/main/java/io/powerrangers/backend/config/Oauth2SuccessHandler.java
@@ -6,27 +6,24 @@ import io.powerrangers.backend.dto.UserDetails;
 import io.powerrangers.backend.entity.RefreshToken;
 import io.powerrangers.backend.entity.User;
 import io.powerrangers.backend.exception.AuthTokenException;
-import io.powerrangers.backend.exception.CustomException;
 import io.powerrangers.backend.exception.ErrorCode;
 import io.powerrangers.backend.service.CookieFactory;
 import io.powerrangers.backend.service.JwtProvider;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -36,7 +33,9 @@ public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     // 추후 userService로 바꾸면 좋을 듯
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
-    private static final String BASE_URL = "http://localhost:8080/index.html";
+
+    @Value("${custom.oauth2.redirect-url}")
+    private String baseUrl;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -65,7 +64,7 @@ public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
         response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
-        String url = UriComponentsBuilder.fromUriString(BASE_URL)
+        String url = UriComponentsBuilder.fromUriString(baseUrl)
                 .queryParam("userId", findUser.getId())
                 .build()
                 .toString();
