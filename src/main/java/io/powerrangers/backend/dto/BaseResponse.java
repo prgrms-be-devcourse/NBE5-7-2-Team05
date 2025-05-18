bdb256d23b0637c904a9671a8474b68e6a30a089
Taeho KIM <laferrari963@naver.com>
2025-05-18 18:00:10 +0900

    Refactor: SuccessCode메시지삭제

--- src/main/java/io/powerrangers/backend/dto/BaseResponse.java
+++ src/main/java/io/powerrangers/backend/dto/BaseResponse.java
@@ -5,6 +5,12 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+/***
+ * ToDo: 에러메시지는 반환, 성공메시지를 삭제하기위해선 두가지 방법이 있어보인다.
+ * 1. message필드를 제외한 성공 리스폰스와, 실패 리스폰스를 따로 관리.
+ * 2. 필드를 유지하고 통합하여 관리하지만 성공 시 message null값 처리.
+ * 일단은 2번이 더 적합하다고 판단하여 진행하였습니다!
+ ****/
 @Getter
 @AllArgsConstructor
 public class BaseResponse<T> {
@@ -15,13 +21,13 @@ public class BaseResponse<T> {
     //성공 메시지만 반환
     public static ResponseEntity<BaseResponse<?>> success(SuccessCode successCode) {
         return ResponseEntity.status(successCode.getStatus())
-                .body(new BaseResponse<>(successCode.getStatus().value(), successCode.getMessage(), null));
+                .body(new BaseResponse<>(successCode.getStatus().value(), null, null));
     }
 
     //성공 메시지와 데이터 반환
     public static <T> ResponseEntity<BaseResponse<T>> success(SuccessCode successCode, T data) {
         return ResponseEntity.status(successCode.getStatus())
-                .body(new BaseResponse<>(successCode.getStatus().value(), successCode.getMessage(), data));
+                .body(new BaseResponse<>(successCode.getStatus().value(), null, data));
     }
 
     //에러 메시지만 반환
--- src/main/java/io/powerrangers/backend/dto/SuccessCode.java
+++ src/main/java/io/powerrangers/backend/dto/SuccessCode.java
@@ -9,16 +9,15 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     // 200 OK
-    MODIFIED_SUCCESS(HttpStatus.OK, "수정이 성공적으로 수행되었습니다."),
-    GET_SUCCESS(HttpStatus.OK, "데이터를 성공적으로 가져왔습니다."),
+    MODIFIED_SUCCESS(HttpStatus.OK),
+    GET_SUCCESS(HttpStatus.OK),
 
     // 201 Created
-    ADDED_SUCCESS(HttpStatus.CREATED, "데이터가 성공적으로 추가되었습니다."),
+    ADDED_SUCCESS(HttpStatus.CREATED),
 
     // 204 No Content
-    DELETED_SUCCESS(HttpStatus.NO_CONTENT, "삭제가 성공적으로 수행되었습니다.");
+    DELETED_SUCCESS(HttpStatus.NO_CONTENT);
 
     private final HttpStatus status;
-    private final String message;
 
 }
