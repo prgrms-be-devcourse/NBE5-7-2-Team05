1b638ff68b634d776749f2a26f2297beba74a160
silver-eunjoo <leeeun5423@naver.com>
2025-05-19 10:14:28 +0900

    Refactor : Repository 단에서 Entity로 받아오게 수정

--- src/main/java/io/powerrangers/backend/dto/UserFollowResponseDto.java
+++ src/main/java/io/powerrangers/backend/dto/UserFollowResponseDto.java
@@ -1,5 +1,6 @@
 package io.powerrangers.backend.dto;
 
+import io.powerrangers.backend.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +13,13 @@ public class UserFollowResponseDto {
     private String nickname;
     private String intro;
     private String profileImage;
+
+    public static UserFollowResponseDto from(User user) {
+        return UserFollowResponseDto.builder()
+                .id(user.getId())
+                .nickname(user.getNickname())
+                .intro(user.getIntro())
+                .profileImage(user.getProfileImage())
+                .build();
+    }
 }
